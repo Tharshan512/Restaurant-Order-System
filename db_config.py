38275f76fda67764c1b4a3fc80d64b5624348bf6
Tharshan512 <tharshan.manimohan@ontariotechu.net>
2026-04-03 12:29:36 -0400

    styling1

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Kartikay.25",
+        password="User2002",
         database="restaurant_db"
     )
     return connection
