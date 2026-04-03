8bf1556f6020e07b8f62f209e1562579b2f15780
nayab-opens <nayab.khan@ontariotechu.net>
2026-04-03 18:55:56 -0400

    styling changes

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Kartikay.25",
+        password="5890Qudsia!",
         database="restaurant_db"
     )
     return connection
