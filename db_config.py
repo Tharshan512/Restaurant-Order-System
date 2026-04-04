b87a5f9efaec1f565d8f13a6e5183ae6fe3492e2
kartikay_sachdeva <kartikay.sachdeva@ontariotechu.net>
2026-04-03 20:03:23 -0400

    Styling Final Version

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="5890Qudsia!",
+        password="Kartikay.25",
         database="restaurant_db"
     )
     return connection
